a547d3c938a118d8dc0b6c82ef0a1a5202a3d445
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2019-02-26 16:23:32 +0000

    :tada: Initial commit.

---
+++ index.js
@@ -0,0 +1,11 @@
+const Entity = require('./src/Entity')
+const Component = require('./src/Component')
+const System = require('./src/System')
+const Manager = require('./src/Manager')
+
+module.exports = {
+  Entity,
+  Component,
+  System,
+  Manager
+}
---
+++ tests/index.js
@@ -0,0 +1,10 @@
+const { expect } = require('chai')
+const ECS = require('../.dev/index.js')
+
+describe('A test suite', () => {
+  describe('A test', () => {
+    it('should run some tests', () => {
+      expect(true).to.be.true
+    })
+  })
+})
