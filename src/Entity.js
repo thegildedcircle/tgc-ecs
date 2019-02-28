eec92b1037d2614138c7009d0c31082de1fba5b5
Andrew Thompson <athom058@gold.ac.uk>
2019-02-28 07:57:12 +0000

    :memo: Update docs.

--- src/Entity.js
+++ src/Entity.js
@@ -33,46 +33,115 @@
  * player.getComponent('health') // { value: 100 }
  * player.findComponents((c => c.state.value >= 50 )) // =>
  * // [
- * //  { name: 'health', unique: true, state: { value: 100 } },
- * //  { name: 'damage', unique: false, state: { value: 50 } }
+ * //   { name: 'health', unique: true, state: { value: 100 } },
+ * //   { name: 'damage', unique: false, state: { value: 50 } }
  * // ]
  * ```
  */
+module.exports = id => new Entity(id)
 
 /**
- * @typedef {Object} Entity
- * 
- * @property {string | number}  name    - .
- * @property {Component[]}      unique  - .
- * @property {function}         addComponent   - .
- * @property {function}         removeComponent   - .
- * @property {function}         hasComponent   - .
- * @property {function}         getComponent   - .
- * @property {function}         findComponents   - .
- * 
+ * @classdesc An Entity is simply an object with an id and a list of all the
+ * components associated with it. The class has a number of methods for adding,
+ * removing, and querying the components stored on it.
  */
-module.exports = id => ({
-  id,
-  components: [],
+class Entity {
+  /**
+   * @param {string | number} id - The unique id of the Entity. Typically id
+   * generation is delegated to the `ECS.Manager`, but if you are rolling your
+   * own you will need to have some method of id generation: Date.now() is the most
+   * simple.
+   * @param {Component[]} [components] - .
+   * 
+   * @description Constructs an Entity with the supplied id, and optionally an
+   * array of initial components. The component list is best used to defined
+   * entity templates, known as an [assemblage](https://entity-system-js.readthedocs.io/en/latest/#assemblages),
+   * and is used by the `ECS.Manager` when calling `ECS.Manager.registerEntityTemplate`.
+   * 
+   * **Note:** You cannot call `new Entity()` directly, instead refer to the 
+   * {@link module:Entity} documentation.
+   * 
+   */
+  constructor(id, components = []) {
+    this.id = id
+    this.components = []
+  }
+  /**
+   * @param {Component} component - .
+   * 
+   * @description Adds a Component to the entity. When attempting to add more than
+   * one instance of a Component that has the `.unique` property set to `true`,
+   * this will print a warning and *not* add the new Component to the components
+   * array.
+   */
   addComponent(component) {
     if (component.unique && this.hasComponent(component.name)) {
       console.warn(`Attempting to add non-unique component "${component.name}" to entity when one already exists.`)
     } else {
       this.components.push(component)
     }
-  },
+  }
+  /**
+   * @param {Component} component - .
+   * 
+   * @description Removes a component from the entity.
+   */
   removeComponent(component) {
     this.components = this.components.filter(c => {
       return JSON.stringify(c) !== JSON.stringify(component)
     })
-  },
+  }
+  /**
+   * @param {string} name - .
+   * 
+   * @return {bool}
+   * 
+   * @description Checks by name if a component exists on the entity. The name
+   * string is matched against the `Component.name` property.
+   */
   hasComponent(name) {
     return this.components.some(c => c.name === name)
-  },
+  }
+  /**
+   * @param {string} name - The name of the Component to query.
+   * 
+   * @return {Component}
+   * 
+   * @description Gets a component stored on the entity by name. If more than one
+   * component name matches, the first is returned.
+   */
   getComponent(name) {
     return this.components.find(c => c.name === name)
-  },
+  }
+  /**
+   * @param {function} predicate - The predicate function to test Components against.
+   * 
+   * @return {Component[]} A list of all Components that match the supplied
+   * predicate
+   * 
+   * @description Gets all components on the entity that satisfy the supplied
+   * predicate function.
+   * 
+   * ```js
+   * const { Entity } = require('ECS')
+   * // Imaginary file that contains some components for our game
+   * const { DamageComponent } = require('./components')
+   * 
+   * const player = Entity(Date.now(), [
+   *   DamageComponent({ value: 50 }),
+   *   DamageComponent({ value: 150 }),
+   *   DamageComponent({ value: 25 }),
+   *   DamageComponent({ value: 5 }),
+   * ])
+   * 
+   * player.findComponents(c => c.name === 'damage' && c.state.value >= 50) // =>
+   * // [
+   * //   { name: 'damage', unique: false, state: { value: 50 } },
+   * //   { name: 'damage', unique: false, state: { value: 150 } }
+   * // ]
+   * ```
+   */
   findComponents(predicate) {
     return this.components.filter(predicate)
   }
-})
+}
